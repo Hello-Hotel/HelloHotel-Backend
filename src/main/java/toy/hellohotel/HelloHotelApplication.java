538cfae1cd4a498bb385b3408210ab916305a4e2
sj <sj@sweettracker.co.kr>
2023-10-16 22:28:38 +0900

    first commit

---
+++ src/main/java/toy/hellohotel/HelloHotelApplication.java
@@ -0,0 +1,13 @@
+package toy.hellohotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/hellohotel/HelloHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.hellohotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
